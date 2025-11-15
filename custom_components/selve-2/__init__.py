2bae051044e290171869ea0123ebcad7834ab65d
archef2000 <69057532+archef2000@users.noreply.github.com>
2025-11-15 21:13:16 +0000

    ignore wrong state updates

--- custom_components/selve-2/__init__.py
+++ custom_components/selve-2/__init__.py
@@ -215,7 +215,41 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             if not dev:
                 continue
 
+            changed_values: list[str] | None = j.get("changed") or None
             udp_state = j.get("state") or {}
+            if changed_values is not None:
+                if len(changed_values) == 7:
+                    all_values = [
+                        "overload",
+                        "obstacle",
+                        "alarm",
+                        "position",
+                        "current",
+                        "target",
+                        "running_state",
+                    ]
+                    if set(changed_values) == set(
+                        all_values
+                    ):  # this is likely a wrong state
+                        run_state = cast(int | None, udp_state.get("run_state", None))
+                        position = cast(int | None, udp_state.get("position", None))
+                        current = cast(int | None, udp_state.get("current", None))
+                        target = cast(int | None, udp_state.get("target", None))
+                        timeout = cast(int | None, udp_state.get("timeout", None))
+                        if (
+                            run_state == 0
+                            and position == 0
+                            and current == 100
+                            and target == 100
+                            and timeout == 0
+                        ):
+                            _LOGGER.warning(
+                                "Selve UDP: ignoring likely wrong state for %s: %s",
+                                sid,
+                                udp_state,
+                            )
+                            continue
+
             # Normalize flags -> attributes for Commeo receivers so binary_sensors update from UDP
             if dev["type"] == "CM":
                 try:
